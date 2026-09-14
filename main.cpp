151c1f2399ff37603b26742f31472a0a8bfac5e1
John Michael Manlangit <johnmichaelmanlangit31@gmail.com>
2026-09-14 11:41:56 +0800

    Update main.cpp via siplasplas

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    int age;
+
+    cout << "Name: " << endl;
+    cin >> name;
+
+    cout << "Age: " << endl;
+    cin >> age;
+
+    cout << "Name: " << name;
+    cout << "Age: " << age;
+    return 0;
+}
